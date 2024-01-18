7930ba6d0478d699bda57893c219e028f7ca1f95
makoge boris <makogeboris@gmail.com>
2024-01-18 02:30:43 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const questions = document.querySelectorAll(".questions");
+const answers = document.querySelectorAll(".answers");
+const icons = document.querySelectorAll(".icons");
+
+questions.forEach((question, index) => {
+  question.addEventListener("click", () => {
+    const isHidden = answers[index].classList.toggle("hidden");
+    hideAllAnswers(index);
+    updateIcons(index, isHidden);
+  });
+});
+
+function hideAllAnswers(exceptIndex) {
+  answers.forEach((answer, index) => {
+    if (index !== exceptIndex) {
+      answer.classList.add("hidden");
+      updateIcons(index, true);
+    }
+  });
+}
+
+function updateIcons(index, isHidden) {
+  icons[index].src = isHidden
+    ? "assets/images/icon-plus.svg"
+    : "assets/images/icon-minus.svg";
+}
